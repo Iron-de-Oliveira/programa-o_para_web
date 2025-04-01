39a56f6b08985205ebe6e88a3643d6fbe578dbc4
Iron-de-Oliveir <irondeoliveira3@gmail.com>
2025-04-01 10:26:15 -0300

    ztualizar

---
+++ programação_para_web_aula4/js/cadastro.js
@@ -0,0 +1,33 @@
+function saveform(){
+    const name = document.getElementById("name").value;
+    const email = document.getElementById("email").value;
+    const senha = document.getElementById("senha").value;
+    const mensagem = document.getElementById("mensagem").value;
+
+    // verificar se existem dados salvos on lovalStorage
+let saveData  = JSON.parse(localStorage.getItem("formData")) || []
+
+// cria um objeto para os dados do formlário
+const FormData  = {
+    name: name,
+    email: email,
+    senha: senha,
+    mensagem: mensagem
+}
+
+// adiciona os dados ao array
+savedData.push(FormData)
+
+// salvar o array no localstorage
+localStorage.setItem("FormData", JSON.stringify(savedData));
+
+// exibir alerta de sucesso
+alert("sucesso")
+
+// lipar fomrulario
+ clearform();
+};
+
+function clearform(){
+    document.getElementsById("mensagem").reset()
+};
